27931bfb0e2413228d31cf255bf2623637e6db63
Norma <normarivasmadrid@gmail.com>
2022-02-02 11:36:09 +0100

    add endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const moviesData = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -10,4 +11,24 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  res.json({
+    success: true,
+    movies: [
+      {
+        id: '1',
+        title: 'Gambita de dama',
+        gender: 'Drama',
+        image: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '2',
+        title: 'Friends',
+        gender: 'Comedia',
+        image: 'https://via.placeholder.com/150',
+      },
+    ],
+  });
 });
