062da240c5b38eb56091752cb73ab0433dc358ac
Norma <normarivasmadrid@gmail.com>
2022-02-03 22:30:18 +0100

    lesson 4.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const moviesData = require('./data/movies.json');
+const users = require('./data/users.json');
 
 // create and config server
 const server = express();
@@ -12,6 +13,14 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// Static Server
+const staticServerPathWeb = './src/public-react'; // Static files
+server.use(express.static(staticServerPathWeb));
+
+// Static Server for images
+const staticServerPathImages = './src/public-movies-images'; // Static files
+server.use(express.static(staticServerPathImages));
 
 //Endpoints
 server.get('/movies', (req, res) => {
@@ -34,6 +43,20 @@ server.get('/movies', (req, res) => {
   });
 });
 
-// Static Server
-const staticServerPathWeb = './public-react'; // Static files
-server.use(express.static(staticServerPathWeb));
+server.post('/login', (req, res) => {
+  console.log(req.body);
+  const loginEmail = req.body.email;
+  const loginPassword = req.body.password;
+  const foundUser = users.find(
+    (user) => user.password === loginPassword && user.email === loginEmail
+  );
+  foundUser //Check if user exists
+    ? res.json({
+        success: true,
+        userId: 'id_de_la_usuaria_encontrada',
+      })
+    : {
+        success: false,
+        errorMessage: 'Usuaria/o no encontrada/o',
+      };
+});
